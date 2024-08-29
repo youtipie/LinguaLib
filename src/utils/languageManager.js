79873dbfdc781f386a015764c7399bff5102e827
youtipie18 <youtipie@gmail.com>
2024-08-29 21:28:18 +0300

    Refactor language getting mechanism

--- src/utils/languageManager.js
+++ src/utils/languageManager.js
@@ -1,18 +1,48 @@
-import countryFlagEmoji from "country-flag-emoji";
 import countryLanguage from "country-language";
 
-const languageData = countryFlagEmoji.list;
+// Had one problem: a lot of countries speak those languages, so flag icon is messed up sometimes.
+// Found one solution: make separate list for popular languages.
+const priorityCountries = {
+    "eng": "US",  // English
+    "spa": "ES",  // Spanish
+    "hin": "IN",  // Hindi
+    "ara": "EG",  // Arabic
+    "fre": "FR",  // French
+    "por": "PT",  // Portuguese
+    "rus": "RU",  // Russian
+    "deu": "DE",  // German
+    "ita": "IT",  // Italian
+    "chi": "CN",  // Chinese
+    "dut": "NL",  // Dutch
+};
 
-// TODO: Find good way of getting languages and their flag icons
-export const languageIconList = languageData.reduce((results, country) => {
-    const lang = countryLanguage.getCountry(country.code).langCultureMs?.[0]?.displayName?.split(" ")[0];
-    if (lang) {
-        results.push({language: lang, icon: country.emoji});
+export const getFlagEmoji = (countryCode) => {
+    if (!countryCode) {
+        return null;
     }
-    return results;
-}, []).sort((a, b) => a.language.localeCompare(b.language));
 
-export const getLanguageByCode = (code) => {
-    const langCode = code.split("-")[0];
-    return countryLanguage.getLanguage(langCode).langCultureMs?.[0]?.displayName?.split(" ")[0] || "Unknown"
+    const codePoints = countryCode
+        .toUpperCase()
+        .split('')
+        .map((char) => 127397 + char.charCodeAt(0));
+    return String.fromCodePoint(...codePoints);
+}
+
+export const languageIconList = countryLanguage.getLanguageCodes(2).reduce((results, languageCode) => {
+    const language = countryLanguage.getLanguage(languageCode);
+    const countryCode = (priorityCountries[languageCode] && priorityCountries[languageCode]) || language.countries?.[0]?.code_2;
+
+    const languageName = language.name?.length && language.name[0];
+    const flagEmoji = getFlagEmoji(countryCode);
+    if (languageName && flagEmoji) {
+        results.push({language: languageName, icon: flagEmoji});
+    }
+
+    return results;
+}, []);
+
+export const getLanguageByCode = (languageCode) => {
+    const sanitizedLanguageCode = languageCode.split("-")[0];
+    const language = countryLanguage.getLanguage(sanitizedLanguageCode);
+    return (language.name?.length && language.name[0]) || "Unknown";
 }
